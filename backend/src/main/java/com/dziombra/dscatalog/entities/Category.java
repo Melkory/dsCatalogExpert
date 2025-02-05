7ad853f8cca8b60f37d983d5ec6e3d5418a97cca
Melkory <carloseduardo.dziombra@hotmail.com>
2025-02-05 14:24:46 -0300

    CategoryResource

---
+++ backend/src/main/java/com/dziombra/dscatalog/entities/Category.java
@@ -0,0 +1,52 @@
+package com.dziombra.dscatalog.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Category {
+
+    @Id
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category( Long id, String name ) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId( Long id ) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals( Object o ) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
