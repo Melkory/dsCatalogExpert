ce34e70e1ccd64a5f735b56431edd620385f36cd
Melkory <carloseduardo.dziombra@hotmail.com>
2025-02-24 11:17:01 -0300

    Product Insert and Update

--- backend/src/main/java/com/dziombra/dscatalog/service/ProductService.java
+++ backend/src/main/java/com/dziombra/dscatalog/service/ProductService.java
@@ -23,6 +23,9 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Transactional(readOnly = true)
     public Page<ProductDTO> findAllPaged (PageRequest pageRequest) {
         Page<Product> result = repository.findAll(pageRequest);
@@ -38,7 +41,7 @@ public class ProductService {
     @Transactional
     public ProductDTO insert( ProductDTO dto ) {
         Product entity = new Product();
-        //entity.setName(dto.getName());
+        copyDtoToEntity(dto, entity);
         entity = repository.save(entity);
         return new ProductDTO(entity);
     }
@@ -47,7 +50,7 @@ public class ProductService {
     public ProductDTO update ( Long id, ProductDTO dto) {
         try {
             Product entity = repository.getReferenceById(id);
-            //entity.setName(dto.getName());
+            copyDtoToEntity(dto, entity);
             entity = repository.save(entity);
             return new ProductDTO(entity);
         } catch (EntityNotFoundException e) {
@@ -66,6 +69,20 @@ public class ProductService {
         }
         catch (DataIntegrityViolationException e) {
             throw new DatabaseException("Falha de integridade referencial");
+        }
+    }
+
+    private void copyDtoToEntity( ProductDTO dto, Product entity ) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setDate(dto.getDate());
+        entity.setImgUrl(dto.getImgUrl());
+        entity.setPrice(dto.getPrice());
+
+        entity.getCategories().clear();
+        for (CategoryDTO catDto : dto.getCategories()) {
+            Category category = categoryRepository.getReferenceById(catDto.getId());
+            entity.getCategories().add(category);
         }
     }
 }
