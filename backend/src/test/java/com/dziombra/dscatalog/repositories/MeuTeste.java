8b27116f7273efc619dcb832a93298a4fcd70137
Melkory <carloseduardo.dziombra@hotmail.com>
2025-03-11 17:07:56 -0300

    Test beforeEach

---
+++ backend/src/test/java/com/dziombra/dscatalog/repositories/MeuTeste.java
@@ -0,0 +1,27 @@
+package com.dziombra.dscatalog.repositories;
+
+import org.junit.jupiter.api.*;
+
+public class MeuTeste {
+
+    @BeforeAll
+    static void setUpBefore () throws Exception {
+    }
+
+    @AfterAll
+    static void tearDownAfterClass () throws Exception {
+    }
+
+    @BeforeEach
+    void setUp () throws Exception {
+    }
+
+    @AfterEach
+    void tearDown () throws Exception {
+    }
+
+    @Test
+    void test () throws Exception {
+    }
+
+}
--- backend/src/test/java/com/dziombra/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/dziombra/dscatalog/repositories/ProductRepositoryTests.java
@@ -2,6 +2,7 @@ package com.dziombra.dscatalog.repositories;
 
 import com.dziombra.dscatalog.entities.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,12 +12,18 @@ import java.util.Optional;
 @DataJpaTest
 public class ProductRepositoryTests {
 
+    private long exintingId;
+
     @Autowired
     private ProductRepository repository;
 
+    @BeforeEach
+    void setUp () throws Exception {
+        exintingId = 1L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists () {
-        long exintingId = 1L;
         repository.deleteById(exintingId);
 
         Optional <Product> result = repository.findById(exintingId);
