f6dd6da65f150168272fd9aa7877294142b27ce2
Melkory <carloseduardo.dziombra@hotmail.com>
2025-03-18 20:37:05 -0300

    Testes com Mockito

--- backend/src/test/java/com/dziombra/dscatalog/service/ProductServiceTests.java
+++ backend/src/test/java/com/dziombra/dscatalog/service/ProductServiceTests.java
@@ -1,9 +1,15 @@
 package com.dziombra.dscatalog.service;
 
 import com.dziombra.dscatalog.repositories.ProductRepository;
+import com.dziombra.dscatalog.service.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -14,5 +20,42 @@ public class ProductServiceTests {
 
     @Mock
     private ProductRepository repository;
+
+    private long existingId;
+    private long nonExistingId;
+
+    @BeforeEach
+    void setUp () throws  Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+
+        Mockito.when(repository.existsById(existingId)).thenReturn(true);
+        Mockito.when(repository.existsById(nonExistingId)).thenReturn(false);
+
+        //Mockito.when(repository.existsById(dependentId)).thenReturn(true);
+
+        Mockito.doThrow(EmptyResultDataAccessException.class)
+                .when(repository).deleteById(nonExistingId);
+
+
+    }
+
+    @Test
+    public void deleteShouldDoNothingWhenIdExists () {
+        Assertions.assertDoesNotThrow(() -> {
+            service.delete(existingId);
+        });
+
+        Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.delete(nonExistingId);
+        });
+
+    }
 
 }
