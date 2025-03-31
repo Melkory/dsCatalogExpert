3df9f81b4b12961faef8da5afa36ddc78f50b27e
Melkory <carloseduardo.dziombra@hotmail.com>
2025-03-31 18:54:50 -0300

    Testes de Integração Delete

---
+++ backend/src/test/java/com/dziombra/dscatalog/service/ProductServiceIT.java
@@ -0,0 +1,44 @@
+package com.dziombra.dscatalog.service;
+
+import com.dziombra.dscatalog.repositories.ProductRepository;
+import com.dziombra.dscatalog.service.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceIT {
+
+    @Autowired
+    private ProductService service;
+
+    @Autowired
+    private ProductRepository repository;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void deleteShouldDeleteResourceWhenIdExistis() {
+        service.delete(existingId);
+        Assertions.assertEquals(countTotalProducts - 1, repository.count());
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.delete(nonExistingId);
+        });
+    }
+
+}
